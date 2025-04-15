8f700df625398e8f82ee37505d6c4639f273e626
zameer <khanzameer5890@gmail.com>
2025-04-15 12:11:27 +0530

    tmdb-player

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import search_icon from '../../assets/search_icon.svg'
@@ -6,8 +6,20 @@ import bell_icon from '../../assets/bell_icon.svg'
 import profile from '../../assets/profile_img.png'
 import caret from '../../assets/caret_icon.svg'
 const Navbar = () => {
+    const navRef = useRef()
+    
+    useEffect(()=>{
+        window.addEventListener('scroll',()=>{
+            if(window.scrollY>=80){
+                navRef.current.classList.add('nav-dark')
+            }else{
+                navRef.current.classList.remove('nav-dark')
+            }
+        })
+    },[])
+
   return (
-    <div  className='nav'>
+    <div  className='nav'  ref={navRef}>
     <div className='nav_left'>
         <img src={logo} alt="" />
         <ul>
--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,12 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Player.css'
+import back_arrow_icon from '../../assets/back_arrow_icon.png'
+import { useNavigate, useParams } from 'react-router-dom'
 
-const Player = () => {
+const Players = () => {
+    const {id} = useParams()
+
+    const navigate = useNavigate()
+    const [apidata,setApidata]=useState({
+        name:"",
+        key:"",
+        published_at:"",
+        type:""
+    })
+
+
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NzRkODgwYjUyZmJlMTI0MmY2MjA0NTI4MTMyMTg2YiIsIm5iZiI6MTc0NDY5NTU4OS4wODUsInN1YiI6IjY3ZmRmMTI1OWQxZjc3OGFiODk5NTQ0YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5j8KUUOQI57NVSeeQquwsPmEESSiltiDzPNLwBTX_qQ'
+      }
+    };
+    
+    useEffect(()=>{
+      fetch(`https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`, options)
+      .then(res => res.json())
+      .then(res => setApidata(res.results[0]))
+      .catch(err => console.error(err)); 
+    },[])
+    
+        
+        
   return (
     <div className='player'>
-      
+        <img src={back_arrow_icon} alt="" onClick={()=>{navigate(-2)}} />
+        <iframe width='90%' height='90%'
+        src={`https://www.youtube.com/embed/${apidata.key}`}
+         title='Trailer' style={{ border: "none" }} allowFullScreen></iframe>
+      <div className="player_info">
+        <p>{apidata.published_at.slice(0,10)}</p>
+        <p>{apidata.name}</p>
+        <p>{apidata.type}</p>
+      </div>
     </div>
   )
 }
 
-export default Player
+export default Players
