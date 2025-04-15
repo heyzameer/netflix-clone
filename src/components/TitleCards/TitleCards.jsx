1a9936f0f667b2a031bff8fc953cc1d56eb5e2d8
zameer <khanzameer5890@gmail.com>
2025-04-15 10:01:56 +0530

    add-homepage-title-component

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-import './TitleCards.css'
-const TitleCards = () => {
-  return (
-    <div className='titlecards'>
-      
-    </div>
-  )
-}
+import React, { useEffect, useRef } from "react";
+import "./TitleCards.css";
+import cards_data from "../../assets/cards/Cards_data";
 
-export default TitleCards
+const TitleCards = ({title,category}) => {
+  const cardsRef = useRef();
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
+
+  useEffect(() => {
+    cardsRef.current.addEventListener("wheel", handleWheel);
+  }, []);
+
+  return (
+    <div className="title-cards">
+      <h2>{title?title:"Populor on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} alt="" />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default TitleCards;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
-import './Home.css'
-import Navbar from '../../components/Navbar/Navbar'
-
+import Nav from '../../components/Navbar/Navbar'
+import TitelCard from '../../components/TitleCards/TitleCards'
+import hero_banner from '../../assets/hero_banner.jpg'
+import hero_titel from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
+import "./Home.css"
+import Footer from '../../components/Footer/Footer'
 const Home = () => {
   return (
-    <div className='home'>
-      <Navbar/>
+    <div className='Home'>
+      <Nav />
+      <div className="hero">
+        <img src={hero_banner} alt="" className='banner_img' />
+        <div className="hero_caption">
+          <img src={hero_titel} alt="" className='caption_img' />
+          <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from on immortal enemy.</p>
+          <div className="hero_btn">
+            <button className='btn'><img src={play_icon} alt="" />Play</button>
+            <button className='btn dark-btn'><img src={info_icon} alt="" />More Info</button>
+          </div>
+          <TitelCard />
+        </div>
+      </div>
+      <div className="more_cards">
+        <TitelCard title={"Blockbuster Movie"} category={"popular"} />
+        <TitelCard title={"Only on Netflix"} category={"top_rated"} />
+        <TitelCard title={"Upcoming"} category={"upcoming"} />
+        <TitelCard title={"Top Pics for you"} category={'now_playing'} />
+      </div>
+      <Footer />
     </div>
   )
 }
